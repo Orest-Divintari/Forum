2a3e5ee3b833e25a88dacc466f3aa7a5991379a9
= <o.divintari@gmail.com>
2020-05-09 13:30:05 +0200

    show trendings only if there are any

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -9,20 +9,20 @@
                 {{ $threads->links() }}
             </div>
         </div>
+        @if(count($trending_threads))
         <div class="col-md-4 mt-3">
             <div class="card">
                 <div class="card-header">Trending Threads</div>
                 <ul class="list-group">
-                    @forelse($trending_threads as $trending_thread)
+                    @foreach($trending_threads as $trending_thread)
                     <li class="list-group-item">
                         <a href="{{ $trending_thread->path }}"> {{ $trending_thread->title}}</a>
                     </li>
-                    @empty
-                    <p>No trending threads yet</p>
-                    @endforelse
+                    @endforeach
                 </ul>
             </div>
         </div>
+        @endif
     </div>
 </div>
 @endsection
