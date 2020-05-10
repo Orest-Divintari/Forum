7294c893de78d8c619c19fdee7b5c7f487639ffe
= <o.divintari@gmail.com>
2020-05-10 13:37:07 +0200

    fixed a bug where the flash message sent from the backend, was not displayed

--- resources/js/components/Flash.vue
+++ resources/js/components/Flash.vue
@@ -1,70 +1,60 @@
 <template>
-    <div
-        class="alert alert-flash"
-        :class="'alert-' + level"
-        role="alert"
-        v-show="show"
-        v-text="body"
-    >
-        <button
-            @click="close"
-            type="button"
-            class="close"
-            data-dismiss="alert"
-            aria-label="Close"
-        >
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
+  <div class="alert alert-flash" :class="'alert-' + level" role="alert" v-show="show" v-text="body">
+    <button @click="close" type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ["message"],
-    data() {
-        return {
-            body: "",
-            show: false,
-            level: "success"
-        };
-    },
+  props: ["message"],
+  data() {
+    return {
+      body: this.message,
+      show: false,
+      level: "success"
+    };
+  },
 
-    created() {
-        if (this.message) {
-            this.flash(this.message);
-        }
-
-        window.events.$on("flash", data => {
-            this.flash(data);
-        });
-    },
-
-    methods: {
-        flash({ message, level }) {
-            this.body = message;
-            this.level = level;
-            this.show = true;
-            this.hide();
-        },
-
-        close() {
-            this.body = "";
-            this.show = false;
-        },
-
-        hide() {
-            setTimeout(() => {
-                this.show = false;
-            }, 3000);
-        }
+  created() {
+    if (this.message) {
+      this.flash();
     }
+
+    window.events.$on("flash", data => {
+      this.flash(data);
+    });
+  },
+
+  methods: {
+    flash(data) {
+      if (data) {
+        this.body = data.message;
+        this.level = data.level;
+      }
+      this.show = true;
+      this.hide();
+    },
+
+    close() {
+      this.body = "";
+      this.show = false;
+    },
+
+    hide() {
+      setTimeout(() => {
+        this.show = false;
+      }, 3000);
+    }
+  }
 };
 </script>
 
 <style>
 .alert-flash {
-    position: fixed;
-    right: 25px;
-    bottom: 2px;
+  position: fixed;
+  right: 25px;
+  bottom: 2px;
 }
 </style>
